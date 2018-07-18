305902e59151d873aec046e714af516f882d24c2
pinniini <joni@pinniini.fi>
2018-07-18 20:14:21 +0300

    Implemented dataloader. Still needs some error handling.

---
+++ src/categorymodel.cpp
@@ -0,0 +1,6 @@
+#include "categorymodel.h"
+
+CategoryModel::CategoryModel()
+{
+
+}
---
+++ src/categorymodel.h
@@ -0,0 +1,13 @@
+#ifndef CATEGORYMODEL_H
+#define CATEGORYMODEL_H
+
+#include <QObject>
+#include <QAbstractListModel>
+
+class CategoryModel : public QAbstractListModel
+{
+public:
+    CategoryModel();
+};
+
+#endif // CATEGORYMODEL_H
